57c2e8f22de8a28e3bb36daba8b5b7667d845978
Tainá Sacramento <tainasacramento2002@gmail.com>
2024-06-17 21:13:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 require("dotenv").config({ path: "./.env" }); //instalr o dotenv: npm install dotenv --save 
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("PORT:", process.env.PORT);
+
+
 const express = require('express');
 const app = express();
 //const port = 3002;
