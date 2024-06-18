b51c83a96393f3a9ac189c99fdb36c9e7df86988
Tainá Sacramento <tainasacramento2002@gmail.com>
2024-06-18 20:06:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3003;
 
 const Usuario = require("./database/Usuario");
 const Aluno = require("./database/Aluno");
